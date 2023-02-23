bb256682370ada3502101c7e34309d60fe98fa68
Riley Zurrin <56184444+RileyZurrin@users.noreply.github.com>
2023-02-23 13:23:10 -0500

    Update Create_phone_number.py

--- Create_phone_number.py
+++ Create_phone_number.py
@@ -1,3 +1,6 @@
+# Prompt: Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+
 def create_phone_number(n):
     s = "".join(map(str,n))
     return "(" + s[0:3] + ") " + s[3:6] + "-" + s[6:10]
